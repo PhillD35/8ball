22ba2061534ad47fe4beef3290749f57fa44d189
phiillD35 <mppodk@mail.ru>
2019-01-09 22:32:10 +0500

    fix code style

--- 8ball.rb
+++ 8ball.rb
@@ -1,37 +1,37 @@
 answers = [
-#Положительные
-"It is certain",
-"It is decidedly so",
-"Without a doubt",
-"Yes — definitely",
-"You may rely on it",
+  #Положительные
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes — definitely",
+  "You may rely on it",
 
-#Нерешительно положительные
-"As I see it, yes",
-"Most likely",
-"Outlook good",
-"Signs point to yes",
-"Yes",
+  #Нерешительно положительные
+  "As I see it, yes",
+  "Most likely",
+  "Outlook good",
+  "Signs point to yes",
+  "Yes",
 
-#Нейтральные
-"Reply hazy, try again",
-"Ask again later",
-"Better not tell you now",
-"Cannot predict now",
-"Concentrate and ask again",
+  #Нейтральные
+  "Reply hazy, try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
 
-#Отрицательные
-"Don’t count on it",
-"My reply is no",
-"My sources say no",
-"Outlook not so good",
-"Very doubtful"
+  #Отрицательные
+  "Don’t count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful"
 ]
 
 hi = [
- "You're not even a CITIZEN! How can you even think of... Fine, I'll talk to you.",
- "Who dusturbs my slumber?! Oh, that's just you...",
- "Oh, Hello! Didn't hear you come in. Greetings and Welcome to Ask That Awesome Magic Ball!"
+  "You're not even a CITIZEN! How can you even think of... Fine, I'll talk to you.",
+  "Who dusturbs my slumber?! Oh, that's just you...",
+  "Oh, Hello! Didn't hear you come in. Greetings and Welcome to Ask That Awesome Magic Ball!"
 ]
 
 puts hi.sample
